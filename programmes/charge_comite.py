c4dbca2e3e1017fbfece1f40912c4e506d1ed0e1
Eric German <german.eric@gmail.com>
2017-11-28 21:57:01 +0100

    add adherent et timestamp

--- programmes/charge_comite.py
+++ programmes/charge_comite.py
@@ -5,18 +5,22 @@
 import sys
 import re
 argv_file= sys.argv[1]
+from datetime import datetime
+madate  = datetime.now()
+strdate = madate.strftime("%Y-%m-%d")
 with open(argv_file,"r") as f:
     lignes = f.readlines()
-    del lignes[0]
+    #del lignes[0]
 hash_com ={}
 for lbrute in lignes:
     ligne = lbrute[0:-1]  # enlever le retour charriot
     tdetail = ligne.split(';')
     #libelle = tdetail[6].replace(chr(39),"''" )
-    id_comite = tdetail[2]
-    url =  tdetail[0]
-    insert = "INSERT INTO COMITES (id_comite,url) VALUES \
-    ('{}', '{}');".format(id_comite,url)
+    id_comite = tdetail[0]
+    url =  tdetail[1]
+    adherents = tdetail[2]
+    insert = "INSERT INTO COMITES (id_comite,url,adherents,datefrom ) VALUES \
+    ('{}', '{}',{},'{}');".format(id_comite,url,adherents,strdate)
     print(insert)
 #for t in hash_com:
 #    print(t)
